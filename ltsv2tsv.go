122c6b4c66ec379c1d3b893e21c1639690aa7863
Morix <rade1500@gmail.com>
2017-08-20 08:48:40 +0000

    first commit

---
+++ ltsv2tsv.go
@@ -0,0 +1,75 @@
+package ltsv2tsv
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+type LTSVConverter struct {
+	reader *bufio.Reader
+}
+
+func NewConverter(r io.Reader) *LTSVConverter {
+	return &LTSVConverter{ bufio.NewReader(r) }
+}
+
+func (r *LTSVConverter) Converter() (records [][]string, err error) {
+	header := map[string]int{}
+	datas := map[int][][]string{}
+	data_index := 0
+
+	// create header record and parse data records.
+	for {
+		line, _, err := r.reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		str_line := strings.TrimSpace(string(line))
+		if str_line == "" {
+			continue
+		}
+
+		line_arr := strings.Split(str_line, "\t")
+		fields := make([][]string, len(line_arr))
+
+		for i, field := range line_arr {
+			field_arr := strings.Split(field, ":")
+
+			_, is_exists := header[field_arr[0]]
+
+			if ! is_exists {
+				header[field_arr[0]] = len(header)
+			}
+			fields[i] = field_arr
+		}
+		datas[data_index] = fields
+		data_index += 1
+	}
+
+	head_record := make([]string, len(header))
+	// from: {"header3": 2, "header1": 0, "header2": 1}
+	// to  : ["header1", "header2", "header3"]
+	for k, v := range header {
+		head_record[v] = k
+	}
+
+	records = make([][]string, data_index + 1)
+
+	for i := 0 ; i < len(datas) ; i++ {
+		if i == 0 {
+			records[i] = head_record
+		}
+		record := make([]string, len(header))
+		for _, fields := range datas[i] {
+			pos, _ := header[fields[0]]
+			record[pos] = fields[1]
+		}
+		records[i+1] = record
+	}
+
+	return
+}
---
+++ ltsv2tsv_test.go
@@ -0,0 +1,64 @@
+package ltsv2tsv
+
+import (
+	"bytes"
+	"testing"
+	"reflect"
+)
+
+var converterTests = []struct {
+	in string
+	out interface{}
+} {
+	{
+		`
+test1:hoge	test2:hoge2	test3:hoge3
+test1:hoge4	test2:hoge5	test3:hoge6
+test2:hoge7	test3:hoge8	test4:hoge9
+		`,
+		[][]string {
+			{"test1", "test2", "test3", "test4"},
+			{"hoge",  "hoge2", "hoge3", ""},
+			{"hoge4", "hoge5", "hoge6", ""},
+			{"",      "hoge7", "hoge8", "hoge9"},
+		},
+	},
+	{
+		`
+test1:hoge	test2:	test3:hoge3
+test1:hoge4	test2:hoge5	test3:
+test2:hoge7	test3:hoge8	test4:hoge9
+		`,
+		[][]string {
+			{"test1", "test2", "test3", "test4"},
+			{"hoge",  "", "hoge3", ""},
+			{"hoge4", "hoge5", "", ""},
+			{"",      "hoge7", "hoge8", "hoge9"},
+		},
+	},
+	{
+		`
+test1:hoge	:hoge2	test3:hoge3
+		`,
+		[][]string {
+			{"test1", "", "test3"},
+			{"hoge",  "hoge2", "hoge3"},
+		},
+	},
+}
+
+func TestConverter(t *testing.T) {
+	for _, v := range converterTests {
+		reader := NewConverter(bytes.NewBufferString(v.in))
+		out, e := reader.Converter()
+		if e != nil {
+			t.Errorf("expected Read got %v", e)
+			continue
+		}
+		if reflect.DeepEqual(out, v.out) == false {
+			t.Errorf("aaa")
+			t.Errorf("Convert(%q) \n %d \n want:\n %d", v.in, out, v.out)
+			continue
+		}
+	}
+}
